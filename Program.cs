58fd33f9deb20fe7101e83473f2b8fb4330883ec
Cristiano Pimentel/cristianopimentel <cristiano.pimentel@hotmail.com>
2018-03-28 12:44:15 -0300

    1st final version. 99% good data.

--- Program.cs
+++ Program.cs
@@ -9,11 +9,67 @@ namespace fair
     public class Card
     {
         public int Id { get; set; }
-        public string Name { get; set; } = "";
+        private string _CardName = "";
         public string Description { get; set; } = "";
         public string List { get; set; } = "";
         public string Comments { get; set; } = "";
         public string ShortUrl { get; set; } = "";
+
+        public string Time { get; private set; }
+        public string Number { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Profile { get; private set; }
+
+        public string CardName
+        {
+            get
+            {
+                return _CardName;
+            }
+            set
+            {
+                _CardName = value.Replace('\t', ' ');
+
+                string[] parts = value.Split('\t');
+                if (parts.Length == 1)
+                    parts = value.Split(' ');
+
+                //  Find email. This "sets" the position for the other data
+                //  This is the simples implementation possible, using "human" algoritm
+                //  Perhaps using RegEx here would extract everything we need
+                int emailPosition;
+                for (emailPosition = 0; emailPosition < parts.Length; emailPosition++)
+                {
+                    if (parts[emailPosition].Contains("@"))
+                    {
+                        this.Email = parts[emailPosition].Trim();
+                        break;
+                    }
+                }
+
+                //  Gets TIME, NUMBER and NAME if they exists. They come in this order.
+                for (int i = emailPosition - 1; i >= 0; i--)
+                {
+                    if (parts[i].Contains(":"))
+                    {
+                        this.Time = parts[i].Trim();
+                    }
+                    else
+                    {
+                        int number = 0;
+                        if (int.TryParse(parts[i].Trim(), out number))
+                            this.Number = number.ToString();
+                        else
+                            this.Name = parts[i] + " " + this.Name;
+                    }
+                }
+
+                //  Gets ROLE
+                for (int i = emailPosition + 1; i < parts.Length; i++)
+                    this.Profile += " " + parts[i];
+            }
+        }
     }
 
     class Program
@@ -45,7 +101,7 @@ namespace fair
                 {
                     Card newCard = new Card();
                     newCard.Id = (int)item["idShort"];
-                    newCard.Name = (string)item["name"];
+                    newCard.CardName = (string)item["name"];
                     newCard.Description = (string)item["desc"];
                     newCard.List = lists[(string)item["idList"]];
                     newCard.ShortUrl = (string)item["shortUrl"];
@@ -76,6 +132,7 @@ namespace fair
                             Dictionary<String, Object> memberCreator = (Dictionary<String, Object>)item["memberCreator"];
 
                             string comment = (string)memberCreator["fullName"] + ": " + (string)data["text"];
+                            comment = comment.Replace("\"", "\"\"").Replace("\n", "").Replace("\r", "");
 
                             if (card.Comments.Length == 0)
                                 card.Comments += comment;
@@ -91,7 +148,7 @@ namespace fair
 
             //  OUTPUT
             //  CardName, CardDesc, Labels, Comments, List, CardLink
-            string[] header = new string[] { "Name", "Description", "List", "Comments", "Card Url" };
+            string[] header = new string[] { "Time", "Number", "Name", "Email", "Profile", "Card Name", "Description", "List", "Comments", "Card Url" };
 
             FileInfo output = new FileInfo(input.FullName + ".csv");
             using (StreamWriter csv = new StreamWriter(File.Open(output.FullName, FileMode.Create), Encoding.UTF8))
@@ -100,14 +157,17 @@ namespace fair
 
                 foreach (Card card in cards.Values)
                 {
-                    string[] record = new string[] { ("\"" + card.Name + "\"").Replace('\t', '*'), "\"" + card.Description + "\"", "\"" + card.List + "\"", "\"" + card.Comments + "\"", card.ShortUrl };
-
-
-                    //  VanHack Spice here
-                    //  Break the Card Name into the fields from event
-
-
-
+                    string[] record = new string[] {
+                        "\"" + card.Time + "\"",
+                        "\"" + card.Number + "\"",
+                        "\"" + card.Name + "\"",
+                        "\"" + card.Email + "\"",
+                        "\"" + card.Profile + "\"",
+                        ("\"" + card.CardName + "\"").Replace('\t', ' '),
+                        "\"" + card.Description + "\"",
+                        "\"" + card.List + "\"",
+                        "\"" + card.Comments + "\"",
+                        card.ShortUrl };
 
                     csv.WriteLine(string.Join(",", record));
                 }
